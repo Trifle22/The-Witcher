0d03b5da55b7aa36df17ccc3792adc89ad22396f
Trifle22 <xromchenkov99@gmail.com>
2020-07-20 16:01:02 +0300

    +adaptive

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 var mySwiper = new Swiper('.swiper-container', {
-    slidesPerView: 2,
+    slidesPerView: 1,
     loop: true,
     navigation: {
         nextEl: '.arrow',
+    },
+    breakpoints: {
+        540: {
+            slidesPerView: 2,
+        }
     }
 });
+
+const menuBtn = document.querySelector('.menu-button');
+const menu = document.querySelector('.header');
+
+
+menuBtn.addEventListener('click', function() {
+    menu.classList.toggle('header-active');
+    menuBtn.classList.toggle('menu-button-active');
+})
